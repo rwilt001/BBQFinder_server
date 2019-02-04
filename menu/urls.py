89d6e145914d0f129ddbef386fe797bb977f3e87
Rafael Wiltz <rwilt001@fiu.edu>
2019-02-03 18:14:28 -0800

    Initial commit

---
+++ menu/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from .views import MenuItemViewSet
+from rest_framework import routers
+
+
+
+router = routers.DefaultRouter()
+router.register(r'menus', MenuItemViewSet)
+#urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
